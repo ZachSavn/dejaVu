491fb9fed9f6767b7020ba32bfad1763175b6dc8
zachary webb <zachsavn@gmail.com>
2022-04-01 15:35:19 -0400

    added required function

--- index.js
+++ index.js
@@ -16,17 +16,24 @@ let answer = Math.floor(Math.random() * 100 + 1);//1 -100
 let guess;
 
 alert("Welcome to the game! Here you shall attempt to guess the allusive number!\n\n To play, select a number between 1 and 100.\n\n Hit Enter to continue.")
-
+//Game Play
 do{
     //Ask for a number
-   guess = prompt("What number are you feeling lucky about?")
-    //Eval answer
-if(guess < answer){
-   alert(`Looks like you undershot the answer ${guess}, try higher higher`)
-} else if (guess > answer){
-    alert(`Looks like you overshot the answer with ${guess}, go lower`)
-}else{
-    alert("You aced the game!!")
-}
+   guess = parseInt(prompt("What number are you feeling lucky about?"))//make sure it's a number
+
+   if (isNaN(guess)){
+       alert("There was no valid number inputted. Please try again")
+   }else{
+       evalAns();
+   }
 }while(answer != guess)
 
+function evalAns(){
+    if(guess < answer){
+        alert(`Looks like you undershot the answer ${guess}, try higher higher`)
+     } else if (guess > answer){
+         alert(`Looks like you overshot the answer with ${guess}, go lower`)
+     }else{
+         alert("You aced the game!!")
+     }
+}

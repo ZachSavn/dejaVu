1981e77ea5f8a394ce746a155cccf51aad539249
zachary webb <zachsavn@gmail.com>
2022-04-04 15:18:17 -0400

    fixed answer variable

--- index.js
+++ index.js
@@ -2,42 +2,40 @@
 
 function startGame(){
     //Intro to game
-alert("Welcome to the Guesing Game! Here you shall attempt to guess the illusive number!\n\n To play, you have to use your wits and correctly guess the number.\n\n Hit Enter to continue.")
-//GAME VALUES
-let answer;
-let difficulty = difficultyLevel();
-let minNum = 1;
-let maxNum;
+    alert("Welcome to the test of intuition.\nYou'll be attempting to guess the number on hand.\nAre you ready for the challenge? Press the Enter key");
+    //Set up and determine difficulty level
+    let difficulty = difficultyLevel();
+    let minNum = 1;
+    let maxNum;
+    if (difficulty == "easy"){
+        maxNum = 10;
+    } else if (difficulty == "medium"){
+        maxNum = 100;
+    } else {
+        maxNum = 1000;
+    }
+    //Generate Answer
+    let answer = randNumRange(minNum, maxNum);
 
-//Set up difficulty
-if (difficulty == "easy"){
-    maxNum = 10;
-}   else if (difficulty == "medium"){a
-    maxNum = 100;
-}        else {
-    maxNum = 1000;
+    console.log(answer, difficulty);
 }
-}
-//Determine Difficulty
-function difficultyLevel(){
-    let difficulty = prompt("Please type your level of difficulty:\n\n easy\nmedium\nhard\n\n Hit Enter to continue.").toLowerCase();
-    
+
+
+function difficultyLevel() {
+    let difficulty = prompt("Type in which difficulty you would like.\n\neasy\nmedium\nhard\n\n [Enter] to continue.").toLowerCase();
     let difficultyPresets = ["easy", "medium", "hard"];
     
     if (difficultyPresets.includes(difficulty)) {
         return difficulty;
     } else {
-        difficultyLevel();
-        alert("Please enter a valid difficulty.Select again.")
+        alert ("You failed to give a valid difficulty. Try again.")
+       return difficultyLevel();
     }
 }
-//set range of numbers for game difficulty
-function randNumInt(min, max){
-    return Math.floor(Math.random() = (max - min)) + min + 1;
+
+
+//generate random number in a range
+function randNumRange(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
 }
-console.log(answer, difficulty)
-
-function playGame(){}
-function endGame(){}
-startGame()
-
+startGame();
